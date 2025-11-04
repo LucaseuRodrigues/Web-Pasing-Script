9d0f3ec80b70fcd9e3acac52a0377c6bfec9eaba
Lucaseu-sys <lucaseurodriguesz@gmail.com>
2025-11-04 18:29:01 -0300

    Att 02: Urllib include
    Getting metadates with urllib requesing and processing datas from the html page.

--- Parsing_Scripts/urllbib.py
+++ Parsing_Scripts/urllbib.py
@@ -1,4 +1,8 @@
 import urllib.request, urllib.parse, urllib.error
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+counts = dict()
 for line in fhand:
-    print(line.decode().strip())
+    word = line.decode().split()
+    for words in word:
+        counts[words] = counts.get(words, 0) + 1
+print(counts)
